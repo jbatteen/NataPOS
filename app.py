1bfb779c664ed01cc9c708ba8c0c963497574862
jbatteen <john@batteen.com>
2024-04-03 01:50:57 -0500

    2404030151

--- app.py
+++ app.py
@@ -9,43 +9,48 @@ from pymongo.mongo_client import MongoClient
 
 
 # import local files
-from load_config import load_config
 from validate_login import validate_login
 from validate_session_key import validate_session_key
 from create_user import create_user
 from config import db_name, mongo_uri, assets_url, business_name
 
-
+# open db
+try:
+  client = MongoClient(mongo_uri)
+except:
+  exit('could not connect to database server') # console
+db = client[db_name]
 
 # begin app
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-  # load config
-#  config = []
-#  config = load_config(db_name, mongo_uri)
-#  business_name = config[0]
-  client = MongoClient(mongo_uri)
-  db = client[db_name]
   if request.method == 'POST':
-    print('post')
-    print('function :' + request.form['function'])
-    if request.form['function'] == 'create_first_user':
-      username = request.form['username']
-      password = request.form['password']
-      password2 = request.form['password']
-      if password != password2:
-        return render_template('create_first_user.html', business_name=business_name, assets_url=assets_url, message="Error: passwords didn't match")
-      else:
-        creation_check = []
-        creation_check = create_user(db, username, password, True)
-        if creation_check == True:
-          return render_template('login.html', business_name=business_name, assets_url=assets_url, message="User created successfully.  You may now log in.")
-        else:
-          return 'error creating user'
+    if request.form['function'] == 'create_first_user': # create first user form submitted
+      try: # if employees collection exists
+        db.validate_collection("employees")
+      except: # if it doesn't which it shouldn't
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password']
+        if username == '':
+          return render_template('create_first_user.html', business_name=business_name, assets_url=assets_url, message="Error: blank username")
+        if password != password2:
+          return render_template('create_first_user.html', business_name=business_name, assets_url=assets_url, message="Error: passwords didn't match")
+        elif password == '':
+          return render_template('create_first_user.html', business_name=business_name, assets_url=assets_url, message="Error: blank password")
 
-    elif request.form['function'] == 'login':
+        else:
+          creation_check = {}
+          creation_check = create_user(db, username, password, True)
+          if creation_check['success'] == True:
+            return render_template('login.html', business_name=business_name, assets_url=assets_url, message="User created successfully.  You may now log in.")
+          else:
+            return render_template('create_first_user.html', business_name=business_name, assets_url=assets_url, message=creation_check['error'])
+      else: # if it does exist and someone called this page anyway
+        return('nice try') # someone spoofed
+    elif request.form['function'] == 'login':  # log in function
       username = request.form['username']
       password = request.form['password']
       try: # if employees collection exists
@@ -53,36 +58,69 @@ def login():
       except: # no collection, try default password
         if username == "admin":
           if password == "admin": # default works, send to create first user page
-            print('admin:admin')
-            return render_template('create_first_user.html', business_name=business_name, assets_url=assets_url)
+            return render_template('create_first_user.html', business_name=business_name, assets_url=assets_url) # default credentials to create first user page
       else: # collection exists, validate
-        print('collection exists')
-        login_check = []
+        login_check = {}
         login_check = validate_login(db, username, password)
-        if login_check[0] == False: # invalid combo, send back to main page
-          return render_template('login.html', business_name=business_name, assets_url=assets_url, error=login_check[1])
+        if login_check['success'] == False: # invalid combo, send back to main page
+          return render_template('login.html', business_name=business_name, assets_url=assets_url, error=login_check['error'])
         else:
-          session_key = login_check[1]
-          return render_template('landing.html', business_name=business_name, assets_url=assets_url, username=username, session_key=session_key)
+          return redirect('/landing/' + login_check['session_key'])
+#          return render_template('landing.html', business_name=business_name, assets_url=assets_url, username=username, session_key=login_check['session_key'])
     else:
 
 
-    # everything failed, send base page
+    # plain old index for the first time
       return render_template('login.html', business_name=business_name, assets_url=assets_url)
 
   else: # if method == GET
     return render_template('login.html', business_name=business_name, assets_url=assets_url)
-@app.route('/landing', methods=['POST'])
-def landing(username, session_key):
-      if request.form['function'] == 'landing':
-        return render_template('landing.html', business_name=business_name, assets_url=assets_url, username=username, session_key=session_key)
-      if request.form['function'] == 'clock_in':
-        return 'clock in'
-      if request.form['function'] == 'clock_out':
-        return 'clock out'
-      if request.form['function'] == 'open_register':
-        return 'open register'
-      if request.form['function'] == 'admin':
-        return 'admin'
+
+
+
+@app.route('/landing/<session_key>', methods=['POST', 'GET'])
+def landing(session_key):
+  result = validate_session_key(db, session_key)
+  if result['success'] == False:
+    return redirect('/')
+  else:
+    username = result['username']
+
+    if request.method == 'GET':
+      timesheet = db.timesheets.find_one({'username': username, 'pay_period': 'current'})
+      if timesheet is not None:
+        worked_hours = float(timesheet['worked_hours'])
+        del timesheet['username']
+        del timesheet['pay_period']
+        del timesheet['worked_hours']
+        del timesheet['_id']
+        timestamps = sorted(timesheet)
+        last_punch = timesheet[timestamps[-1]]
+        message = 'Last punch: ' + last_punch + " " + time.ctime(int(timestamps[-1]))
+        if len(timesheet) > 1:
+          second_to_last_punch = timesheet[timestamps[-2]]
+          if last_punch == second_to_last_punch:
+            message = message + "<br>Missing punch, see a manager.<br>Second to last punch: " + second_to_last_punch + " " + time.ctime(int(timestamps[-2]))
+      else:
+        message = 'First day worked in the pay period!'
+
+
+      return render_template('landing.html', business_name=business_name, assets_url=assets_url, username=username, session_key=session_key, message=message)
+
+
+    if request.form['function'] == 'clock_in':
+      current_time = int(round(time.time()))
+      timesheet = db.timesheets.find_one({'username': username, 'pay_period': 'current'})
+      if timesheet is not None:
+        db.timesheets.update_one({'username': username, 'pay_period': 'current'}, {'$set': {str(current_time): 'in'}})
+      else:
+        db.timesheets.insert_one({'username': username, 'pay_period': 'current', 'worked_hours': 0.0, str(current_time): 'in'})
+      return redirect('/landing/' + session_key)
+    if request.form['function'] == 'clock_out':
+      return 'clock out'
+    if request.form['function'] == 'open_register':
+      return 'open register'
+    if request.form['function'] == 'admin':
+      return 'admin'
 
 
--- validate_login.py
+++ validate_login.py
@@ -1,19 +1,21 @@
 from pymongo.mongo_client import MongoClient
 import bcrypt
+import random
+import time
 
 def validate_login(db, username, password):
-  try:  # if employee collection exists
-    db.validate_collection("employees")
-  except: # no collection, fail
-    return [False, "error: sample error"]
-  else: # validate login
-    document = db.employees.find_one({"username" : username})
-    print('validate login')
+  document = db.employees.find_one({'username' : username})
+  if document is not None:
     check_hash = password.encode('utf-8')
     hash = document['password']
     password_ok = bcrypt.checkpw(check_hash, hash)
-    print('password_ok: ' + str(password_ok))
     if password_ok == True:
-      return [True, "sample_session_key"]
+      db.session_keys.delete_many({'username': username})
+      session_key = ''.join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890") for _ in range(40))
+      time_int = int(round(time.time()))
+      result = db.session_keys.insert_one({'session_key': session_key, 'time_stamp': time_int, 'username': username})
+      return {'success': True, 'session_key': session_key}
     else:
-      return [False, "wrong password"]
+      return {'success': False, 'error': 'wrong password'}
+  else:
+    return {'success': False, 'error': 'no such user in system'}
